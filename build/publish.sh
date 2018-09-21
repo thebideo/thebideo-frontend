c4ddfe798876d6728bcbabe7925c8aff23b09847
Nick Thorp <nickthorp20@gmail.com>
2018-09-20 23:28:47 -0500

    updating some backup and publish steps

--- build/publish.sh
+++ build/publish.sh
@@ -6,8 +6,8 @@ BACKUPDIR='/opt/backup_thebideo-frontend/'
 echo 'Cleaning up backup dir'
 rm -rf $BACKUPDIR*
 echo 'Backing up'
-mv $WEBDIR/* $BACKUPDIR
+tar czvf $BACKUPDIR/backup_thebideo-frontend.tar.gz -C $WEBDIR .
 echo 'Deploying'
 cd ..
-rsync -av --progress . $WEBDIR --exclude build --exclude build@tmp
+rsync -av --progress . $WEBDIR --exclude 'build' --exclude 'build@tmp'
 echo 'Done!'
