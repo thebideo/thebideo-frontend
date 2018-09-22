b7deab9014c938f80c9ec75862c6ee891efc648c
Nick Thorp <nickthorp20@gmail.com>
2018-09-22 18:31:10 -0500

    Changing publishing method

--- build/publish.sh
+++ build/publish.sh
@@ -6,8 +6,8 @@ BACKUPDIR='/opt/backup_thebideo-frontend/'
 echo 'Cleaning up backup dir'
 rm -rf $BACKUPDIR*
 echo 'Backing up'
-tar czvf $BACKUPDIR/backup_thebideo-frontend.tar.gz -C $WEBDIR .
+tar czvf $BACKUPDIR/backup_thebideo-frontend.tar.gz -C $WEBDIR . --exclude=*.mp3
 echo 'Deploying'
-cd ..
-rsync -av --progress . $WEBDIR --exclude 'build' --exclude 'build@tmp'
+cd $WORKSPACE
+cp -rf *.html *.css assets/ favicon.ico *.js $WEBDIR
 echo 'Done!'
