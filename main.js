007ca4bf3f48f2ace9fea616d9f84cd8d18159d4
Nick Thorp <nickthorp20@gmail.com>
2018-09-23 16:10:04 -0500

    Updating some navbar stuff\nAdding hamburger menu logos

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log('Hello world!')
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar col-xs-9 col-md-9") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar col-xs-9 col-md-9";
+    }
+    console.log("I mean, I got executed...")
+} 
