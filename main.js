4da954e24354e1d102c4921487d1a1faf175420c
Nick Thorp <nickthorp20@gmail.com>
2018-09-27 23:53:12 -0500

    Fixing Logo positioning

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("navbar");
-    if (x.className === "navbar col-xs-9 col-md-9") {
+    if (x.className === "navbar col-xs-12 col-sm-12") {
         x.className += " responsive";
     } else {
-        x.className = "navbar col-xs-9 col-md-9";
+        x.className = "navbar col-xs-12 col-sm-12";
     }
 } 
